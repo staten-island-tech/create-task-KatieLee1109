fa8d37fba3f20fc5b98629c3362a36dfee392715
KatieLee1109 <113190058+KatieLee1109@users.noreply.github.com>
2023-02-15 10:16:10 -0500

    commit 3

--- AP Create Task/js/main.js
+++ AP Create Task/js/main.js
@@ -1,2 +1,7 @@
 import './style.css'
+let input = document.querySelectorAll('.search')
+let
+    let data = await fetch("https://openlibrary.org/authors/OL1394244A/works.json?limit=100");
+console.log(data);
+
 
