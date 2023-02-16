5b97efebf4b4cef3ab1b8a3f8e3b105c509f120a
KatieLee1109 <113190058+KatieLee1109@users.noreply.github.com>
2023-02-16 10:16:19 -0500

    Commit 4

--- AP Create Task/js/main.js
+++ AP Create Task/js/main.js
@@ -1,7 +1,27 @@
-import './style.css'
+import "style"
 let input = document.querySelectorAll('.search')
-let
+let form = document.querySelectorAll('.submit')
+let p = document.querySelectorAll('.display')
+form.addEventListener("click", (e) => {
+    e.preventDefault();
+    let inputval = input.value;
+    display(inputval);
+});
+async function callAPI(value) {
     let data = await fetch("https://openlibrary.org/authors/OL1394244A/works.json?limit=100");
-console.log(data);
+    let returned = await data.json();
+    return returned;
+};
+async function display(value) {
+    let data = await callAPI(value);
+    console.log(data);
+    data.data.forEach(element => {
+        p.insertAdjacentHTML(
+            'afterend',
+            `<p class ="yay">${element.title}
+            <button class="choose">Save</button>
+        </p>`)
+    });
+};
 
 
