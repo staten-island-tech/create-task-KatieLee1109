06d6eabd7fce0fbd5b2ec9db81675f6c13299129
KatieLee1109 <113190058+KatieLee1109@users.noreply.github.com>
2023-02-27 18:39:51 -0500

    Commit 8

--- AP Create Task/js/main.js
+++ AP Create Task/js/main.js
@@ -1,99 +1,54 @@
-import { books } from "./file";
-let input = document.querySelectorAll(".search")
-let display = document.querySelectorAll(".display")
-let form = document.querySelectorAll(".searchform")
-
-form.addEventListener("click", (e) => {
-    e.preventDefault();
-    let inputval = input.value;
-    display(inputval);
-});
-async function callfile(value) {
-    value.forEach(books) => {
-        display.innerHTML = "";
-        (
-            "beforeend",
-            `<h2> ${books.title}</h2>
-            <p id="number">Published: ${books.year}</p>
-        <img class = "img" src = ${books.img} alt=""/>
-        <p id="text">Description: ${books.description}</p>
-        <button id="order">Buy the Book</button>
-        `
-        )
-    };
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// async function response(value) {
-
-// }
-
-
-
-// async function books(things) {
-//     try {
-//         const response = await (things);
-//         if (response.status < 200 || response.status > 299) {
-//             throw new error(response);
-//         } else {
-//             const data = await response.json();
-//             console.log(data);
-//             DOMSelectors.form.insertAdjacentElement(
-//                 "beforeend",
-//                 ``
-//             )
-//         }
+// import {books} from "./file"
+// const submitinput = document.querySelectorAll('.input')
+// submitinput.addEventListener("input", (e) => {
+// async function getresponse(books)
+//     let value = e.target.value
+//     if (value.status < 200 || value.status > 299){
+//         throw console.error(response);
+//     }else{
+//         const data = await response.json();
+//         console.log(data);
 //     }
+// })
+   
+
+import { books } from "./file";
+import "../style.css";
+const DomSelectors = {
+    form: document.querySelectorAll(".form"),
+    button1: document.querySelectorAll(".submit"),
+    button2: document.querySelectorAll(".clear"),
+    input: document.querySelectorAll(".input"),
+}
+DomSelectors.form.addEventListener("input", (e) => {
+    e.preventDefault();
+    let inputVal = input.value;
+    display(inputVal)
+});
+async function file(){
+    const response = await fetch (books);
+    const books = await response.json();
+    return books;
+}
+file().then(books => {
+    books;
+});
+
+async function error(){
+    const response = await fetch (books);
+    if (response){
+        const message = `The book you have searched up does not exist in the system.`;
+        throw new error(message);
+    }
+    const books = await response.json();
+    return books;
+}
+error().catch(error => {
+    error.message;
+});
 
 
 
-//  const error()
-//     console.log(error);
-//     DOMSelectors.input.insertAdjacentElement(
-//         "beforeend",
-//         `<div class="popup">
-//         <p> The book you have searched for is not in the system.<p>
-//         <div>`
-//     );
-// };
+
+
+
