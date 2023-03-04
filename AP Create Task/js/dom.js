cd260520637873e39b4bab67ad5670524b06bf43
KatieLee1109 <113190058+KatieLee1109@users.noreply.github.com>
2023-03-03 21:27:55 -0500

    Commit 10

--- AP Create Task/js/dom.js
+++
@@ -1,7 +0,0 @@
-const DOMSelectors = {
-    form: document.querySelectorAll(".form"),
-    button1: document.querySelectorAll(".submit"),
-    input: document.querySelectorAll(".input"),
-    q: document.querySelectorAll(".display"),
-};
-export { DOMSelectors };
--- AP Create Task/js/main.js
+++ AP Create Task/js/main.js
@@ -1,34 +1,41 @@
-import { books } from "./file"
-import { DOMSelectors } from "./dom"
-DOMSelectors.form.addEventListener("submit", function (e) {
-    e.preventDefault();
-    let inputVal = input.title;
-    display(inputVal)
-});
-async function file(booked) {
-    const book = await fetch(books)
-    console.log(book)
-    const returned = await booked.json();
-    return returned;
+import {books} from "./file";
+
+const DOMSelectors = {
+    display: document.querySelectorAll("display"),
+    btn1: document.querySelectorAll("btn"),
+    showbook: document.querySelectorAll("book"),
 };
-async function display(booked) {
-    const book = await file(booked);
-    console.log(book)
-    books.forEach(books => {
-        q.insertAdjacentHTML(
-            "afterend",
-            `<img class="img" src= ${books.img} alt=""/>
-             <p class="output">Year Published: ${books.year}<p>
-             <p class="output">Description: ${books.description}<p>
-             <p class="buy">Purchase the Book: ${books.buy}</p>
-             `)
+
+const book = fetch (books)
+
+async function getdata(book){
+    try{
+        const response = await fetch (book);
+        if (response.status < 200 || response.status > 299){
+            throw new Error(response);
+        } else{
+            const data = await response.json();
+            console.log(data);
+
+function createcard(){
+    books.forEach((books) => {
+        DOMSelectors.display.insertAdjacentHTML 
+        ("afterbegin",
+        `<h1>${books.title}<h1>
+        <img class="img" src= ${books.img} alt = ""/>
+        <h2>${books.description}<h2>
+        <button class="btn2"><button>
+        `);
     });
-    q.innerHTML = title.booked
-};
-
-
-
-
-
-
+}
+DOMSelectors.book.addEventListener("click", function(){
+    createcard();
+});
+        }
+}catch(error){
+    console.log(error);
+    console.log("The book can't be found");
+}
+}
+getdata(books);
 
